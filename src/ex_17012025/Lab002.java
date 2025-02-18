14e4ef5949669c955467aaf49c998c80fb8e68cf
gayathrilatha-d <gayathridevathi@gmail.com>
2025-02-18 17:50:59 +0530

    adding files

--- src/ex_17012025/Lab002.java
+++ src/ex_17012025/Lab002.java
@@ -3,6 +3,7 @@ package ex_17012025;
 public class Lab002 {
     public static void main(String[] args) {
        System.out.println("Lab");
+        System.out.println("Lab01");
 
     }
 }
---
+++ src/ex_17012025/Lab003.java
@@ -0,0 +1,15 @@
+package ex_17012025;
+
+public class Lab003 {
+        public static void main(String[] args) {
+        byte b= 124;
+        short s= 101;
+        int Num= 110033;
+        char name= 'A';
+        float f= 11.22f;
+        long rest= 9988665544L;
+        double yum= 883939;
+
+        System.out.println(yum);
+    }
+}
