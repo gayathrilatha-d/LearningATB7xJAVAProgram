58bc81461adfc13b116e617311a2adbb16a26315
gayathrilatha-d <gayathridevathi@gmail.com>
2025-02-18 18:07:14 +0530

    adding files

---
+++ src/ex_17012025/Lab005.java
@@ -0,0 +1,11 @@
+package ex_17012025;
+
+public class Lab005 {
+    public static void main(String[] args) {
+        char r= '\r';
+        char t='\t';
+        System.out.println("Pramod"+r+"Datta" );
+        System.out.println("Pramod"+t+"Datta" );
+
+    }
+}
