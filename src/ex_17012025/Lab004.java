c00c47566aa8eac5a8467c03c539f21faf58edcc
gayathrilatha-d <gayathridevathi@gmail.com>
2025-02-18 18:01:29 +0530

    adding files

---
+++ src/ex_17012025/Lab004.java
@@ -0,0 +1,15 @@
+package ex_17012025;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        int a=13;
+        int b=30;
+        char y='w';
+        String l= "Joke";
+        System.out.println(a+y);
+        System.out.println(a+l);
+        System.out.println(b+y+l);
+        System.out.println(y+l);
+        System.out.println(l+y);
+    }
+}
