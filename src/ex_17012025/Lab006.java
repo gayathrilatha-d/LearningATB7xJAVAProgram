56ca2952f5fe2caa0b4c7f43b97d573a9f248fd4
gayathrilatha-d <gayathridevathi@gmail.com>
2025-02-18 18:38:21 +0530

    adding files

---
+++ src/ex_17012025/Lab006.java
@@ -0,0 +1,19 @@
+package ex_17012025;
+
+public class Lab006 {
+    public static void main(String[] args) {
+        int a=100;
+        int b=1000;
+        int c=333;
+        System.out.println(a+b);
+        System.out.println(a+c);
+        System.out.println(a-c);
+      //  System.out.println(a/b);
+        System.out.println(a%c);
+       // System.out.println(a=b=c);
+        //System.out.println(a=b);
+        //System.out.println(b=a);
+        //System.out.println(a=c);
+        //System.out.println(c==a);
+    }
+}
